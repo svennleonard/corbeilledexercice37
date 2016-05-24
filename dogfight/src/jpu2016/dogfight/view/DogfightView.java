3adcf7dc9cfc18076f7381d3c56e4bdc8f16bdfb
Kirakiira <gintokisakata33@gmail.com>
2016-05-24 11:15:14 +0200

    fcbrfeclj

---
+++ dogfight/src/jpu2016/dogfight/view/DogfightView.java
@@ -0,0 +1,10 @@
+package jpu2016.dogfight.view;
+
+public class DogfightView {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ dogfight/src/jpu2016/gameframe/GameFrame.java
@@ -0,0 +1,10 @@
+package jpu2016.gameframe;
+
+public class GameFrame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
